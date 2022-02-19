b65d55b9f7aa16c8277b7f89bd85dbc056217c6d
SLOM2539 <94549888+SLOM2539@users.noreply.github.com>
2022-02-20 01:08:29 +0300

    Update _asroz.py

--- Jmthon/razan/resources/assistant/_asroz.py
+++ Jmthon/razan/resources/assistant/_asroz.py
@@ -3,9 +3,9 @@ from userbot.Config import Config
 RBOT = Config.TG_BOT_USERNAME
 ROZA = Config.PRIVATE_GROUP_BOT_API_ID
 StJms = [
-    "▾∮ جـار اعـداد البـوت المسـاعد",
-    "▾∮ يـتم اكمـال اعـداد البـوت االمسـاعد",
-    "▾∮ تـم بنجاح اعـداد البـوت المسـاعد",
+    "• جار اعداد البوت المساعد",
+    "• يتم اكمال اعداد البوت المساعد",
+    "• ابشر تم بنجاح اعداد البوت المساعد",
 ]
-Stdec = "- 𝖩𝖬𝖳𝖧𝖮𝖭 𝖴𝖲𝖤𝖱𝖡𝖮𝖳 𝖠𝖲𝖲𝖨𝖳𝖠𝖭𝖳 \n- 𝖩𝖬𝖳𝖧𝖮𝖭 𝖥𝖮𝖱  {}\n- @JMTHON "
-SetAbt = "- 𝖩𝖬𝖳𝖧𝖮𝖭 𝖴𝖲𝖤𝖱𝖡𝖮𝖳\n- 𝖩𝖬𝖳𝖧𝖮𝖭 𝖥𝖮𝖱  {}"
+Stdec = "- Fawaz 𝖴𝖲𝖤𝖱𝖡𝖮𝖳 𝖠𝖲𝖲𝖨𝖳𝖠𝖭𝖳 \n- Fawaz 𝖥𝖮𝖱  {}\n- @UKKKP"
+SetAbt = "- Fawaz 𝖴𝖲𝖤𝖱𝖡𝖮𝖳\n- Fawaz 𝖥𝖮𝖱  {}"
